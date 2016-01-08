f9bd009d73b7f7d560de7f615fd7a07d81eefabe
Shane Spoor <shane.d.spoor@gmail.com>
2016-01-08 04:02:40 -0800

    Remove accordion animation for now

--- js/accordion.js
+++ js/accordion.js
@@ -1,8 +1,15 @@
 // Script for 
 $(document).ready(function($) {
-    $('.accordion').find('.accordion-toggle').click(function(){
+    var toggles = $('.accordion').find('.accordion-toggle');
+
+    // Make the contents of the first toggle visible by default
+    toggles.first().next('.accordion-content').addClass('default');
+    toggles.first().find('.accordion-indicator').text('-');
+
+    // Add a method to expand the panel on click    
+    toggles.click(function(){
         // Expand or collapse this panel
-        $(this).next().slideToggle('fast');
+        $(this).next('.accordion-content').toggle();
         
         var indicatorChar = $(this).find('.accordion-indicator').text();
         indicatorChar = indicatorChar == '+' ? '-' : '+';
